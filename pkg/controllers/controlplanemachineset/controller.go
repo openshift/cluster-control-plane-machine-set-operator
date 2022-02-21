64475516752066387314eabbd3a86a145344364b
Joel Speed <joel.speed@hotmail.co.uk>
2022-02-21 14:55:30 +0000

    Add basic ControlPlaneMachineSet controller
    This adds a very basic ControlPlaneMachineSet controller that will later 
    be populated

---
+++ pkg/controllers/controlplanemachineset/controller.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2022 Red Hat, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controlplanemachineset
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+// ControlPlaneMachineSetReconciler reconciles a ControlPlaneMachineSet object
+type ControlPlaneMachineSetReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// Reconcile reconciles the ControlPlaneMachineSet object
+func (r *ControlPlaneMachineSetReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ControlPlaneMachineSetReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		// Uncomment the following line adding a pointer to an instance of the controlled resource as an argument
+		// For().
+		Complete(r)
+}
